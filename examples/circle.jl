f1c539a3d03c1af3bae9b154f81398ec439ba563
Site Wang <sitew@clemson.edu>
2017-08-04 12:38:09 -0600

    test added

--- examples/circle.jl
+++ examples/circle.jl
@@ -1,13 +1,13 @@
 using JuMP, MathProgBase, Gurobi, Ipopt, POD
 
-function circle(;verbose=false, solver=nothing, bt=false)
+function circle(;verbose=false, solver=nothing)
 
 	if solver == nothing
 		m = Model(solver=PODSolver(nlp_local_solver=IpoptSolver(print_level=0),
 								   mip_solver=GurobiSolver(OutputFlag=0),
                                    discretization_width_tol=1e-2,
                                    discretization_ratio=4,
-								   presolve_bound_tightening=bt,
+								   presolve_bound_tightening=false,
 								   presolve_bound_tightening_algo=1,
 								   presolve_bt_output_tol=1e-1,
 								   log_level=1))
