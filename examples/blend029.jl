27de77e6a0881cde963d4b31fc37361fcf2a123b
jac0320 <sitew@g.clemson.edu>
2017-09-07 13:02:49 -0600

    Debugging

--- examples/blend029.jl
+++ examples/blend029.jl
@@ -1,10 +1,12 @@
-using POD, JuMP, CPLEX, Ipopt, MathProgBase, AmplNLWriter, CoinOptServices, Pajarito
+using POD, JuMP, Gurobi, Ipopt, MathProgBase, AmplNLWriter, CoinOptServices, Pajarito
 
 function blend029(;verbose=false, solver=nothing, convhull=true, exprmode=1, sos2=true, presolve=0, dynamic=false, level=0.8, minimum=1)
 
     if solver == nothing
-        m = Model(solver=PODSolver(minlp_local_solver=PajaritoSolver(mip_solver=mip_solver, cont_solver=nlp_solver),
-                                    mip_solver=CplexSolver(CPX_PARAM_SCRIND=1),
+        m = Model(solver=PODSolver( #minlp_local_solver=PajaritoSolver(mip_solver=GurobiSolver(OutputFlag=0), cont_solver=IpoptSolver()),
+                                    minlp_local_solver=BonminNLSolver(["bonmin.iteration_limit=100"; "bonmin.num_resolve_at_root=5"; "bonmin.num_resolve_at_node=5"; "bonmin.nlp_log_level=0"; "bonmin.bb_log_level=0"]),
+                                    nlp_local_solver=IpoptSolver(print_level=0),
+                                    mip_solver=GurobiSolver(OutputFlag=0),
                                     presolve_bound_tightening=(presolve>0),
                                     presolve_bound_tightening_algo=presolve,
                                     bilinear_convexhull=false,
@@ -14,8 +16,7 @@ function blend029(;verbose=false, solver=nothing, convhull=true, exprmode=1, sos
                                     discretization_var_minimum=minimum,
 									convhull_formulation_sos2=sos2,
                                     discretization_ratio=8,
-                                    # discretization_var_pick_algo="min_vertex_cover",
-                                    log_level=1,
+                                    log_level=100,
                                     rel_gap=0.0001))
     else
         m = Model(solver=solver)
