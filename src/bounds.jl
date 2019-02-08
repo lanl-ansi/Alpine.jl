50c6524bb36571070e39db2bf032e0270e84cba9
Harsha Nagarajan <harsha@Harshas-MacBook-Pro.local>
2019-02-08 14:14:12 -0700

    bound bug fix

--- src/bounds.jl
+++ src/bounds.jl
@@ -299,12 +299,12 @@ function resolve_inf_bounds(m::AlpineNonlinearModel)
     for i in m.candidate_disc_vars
         if m.l_var_tight[i] == -Inf
             warnuser = true
-            # m.l_var_tight[i] = -m.largebound
+            m.l_var_tight[i] = -m.largebound
             infcount += 1
         end
         if m.u_var_tight[i] == Inf
             warnuser = true
-            # m.u_var_tight[i] = m.largebound
+            m.u_var_tight[i] = m.largebound
             infcount +=1
         end
     end
