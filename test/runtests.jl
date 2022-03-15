ad07cbf6da830fbaf22ffada2bce59843d468ca8
odow <o.dowson@gmail.com>
2022-03-15 15:39:18 +1300

    Simplify test dependencies

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,28 +1,21 @@
-using JuMP, MathOptInterface
-using LinearAlgebra
-using Ipopt
-using Cbc
-using Juniper
-import Pavito
-using GLPK
-using Alpine
+using JuMP
 using Test
-using Random
 
-alpine_dir = joinpath(dirname(pathof(Alpine)), "..")
+import Alpine
+import Cbc
+import Ipopt
+import Juniper
+import Pavito
 
-examples = readdir(joinpath(alpine_dir, "examples", "MINLPs"))
-
-for i in examples
-    include(joinpath(alpine_dir, "examples", "MINLPs", i))
+const _EXAMPLES_DIR = joinpath(@__DIR__, "..", "examples", "MINLPs")
+for file in readdir(_EXAMPLES_DIR)
+    include(joinpath(_EXAMPLES_DIR, file))
 end
 
-const MOI = MathOptInterface
-
-const IPOPT   = optimizer_with_attributes(Ipopt.Optimizer, MOI.Silent() => true, "sb" => "yes", "max_iter" => 9999)
-const CBC     = optimizer_with_attributes(Cbc.Optimizer, MOI.Silent() => true)
-const JUNIPER = optimizer_with_attributes(Juniper.Optimizer, MOI.Silent() => true, "mip_solver" => CBC, "nl_solver" => IPOPT)
-const PAVITO  = optimizer_with_attributes(Pavito.Optimizer, MOI.Silent() => true, "mip_solver" => CBC, "cont_solver" => IPOPT, "mip_solver_drives" => false)
+const IPOPT   = MOI.OptimizerWithAttributes(Ipopt.Optimizer, MOI.Silent() => true, "sb" => "yes", "max_iter" => 9999)
+const CBC     = MOI.OptimizerWithAttributes(Cbc.Optimizer, MOI.Silent() => true)
+const JUNIPER = MOI.OptimizerWithAttributes(Juniper.Optimizer, MOI.Silent() => true, "mip_solver" => CBC, "nl_solver" => IPOPT)
+const PAVITO  = MOI.OptimizerWithAttributes(Pavito.Optimizer, MOI.Silent() => true, "mip_solver" => CBC, "cont_solver" => IPOPT, "mip_solver_drives" => false)
 
 function _build(model::JuMP.Model)
     MOI.set(model, MOI.NLPBlock(), JuMP._create_nlp_block_data(model))
